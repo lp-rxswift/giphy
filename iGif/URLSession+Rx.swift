5e34781361be442991997506ae55c371fcad98f2
Lucas <lucaspedrazoli@gmail.com>
2021-01-14 15:06:47 -0300

    implement response method for URLSession wrapper

--- iGif/URLSession+Rx.swift
+++ iGif/URLSession+Rx.swift
@@ -13,7 +13,20 @@ extension Reactive where Base: URLSession {
   func response(request: URLRequest)
   -> Observable<(HTTPURLResponse, Data)> {
     return Observable.create { observer in
-      return Disposables.create()
+      let task = self.base.dataTask(with: request) { data, response, error in
+        guard let response = response, let data = data else {
+          observer.onError(error ?? RxURLSessionError.unknown)
+          return
+        }
+        guard let httpResponse = response as? HTTPURLResponse else {
+          observer.onError(RxURLSessionError.invalidResponse(response: response))
+          return
+        }
+        observer.onNext((httpResponse, data))
+        observer.onCompleted()
+      }
+      task.resume()
+      return Disposables.create { task.cancel() }
     }
   }
 }
